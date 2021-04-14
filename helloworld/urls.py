5bf1cacf17f24f21a24c4495525084f76b8d9c62
Cristian Zambrano <cc.zambrano2471@gmail.com>
2021-04-13 20:49:32 -0500

    Simultaneous commit in master

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fecha', views.hello_time),
+    path('', views.hello_time),
     path('<int:year>/', views.hello_year),
     path('json/<slug:data>/', views.json_data, name='json data'),
 ]
